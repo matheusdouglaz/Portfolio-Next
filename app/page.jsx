0268d721cdf0e36bb755850235aaed2af79781ba
matheusdouglaz <matheustecnologi@outlook.com>
2024-08-02 16:27:13 -0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button"
+import {FiDownload} from "react-icons/fi"
+
+const Home = () => {
+  return (
+    <section className="h-full">Home</section>
+  )
+};
+
+export default Home;
