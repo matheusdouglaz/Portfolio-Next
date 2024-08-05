f0ae7aa9f550af4ade06af5aded2dfffaaebb6fd
matheusdouglaz <matheustecnologi@outlook.com>
2024-08-05 19:10:19 -0300

    Pagina Home finalizada

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,6 @@
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
 import {Button} from "@/components/ui/button"
 import {FiDownload} from "react-icons/fi"
 
@@ -7,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/*text*/}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Desenvolvedor Front-End</span>
             <h1 className="h1 mb-6">
               Hello I&apos;m <br/> <span className="text-accent">Matheus Alves</span>
@@ -20,14 +23,17 @@ const Home = () => {
                 <FiDownload className="text-xl"/>
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Socials />
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration:500"/>
                 </div>
             </div>
           </div>
           {/*photo*/}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
